97173f6d7cc41954e69cdf8009c4311a8c23d426
jennyjunho2 <a01032208149@gmail.com>
2022-06-14 22:43:36 +0900

    220614

---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components'
+
+module 'styled-components' {
+    export interface DefaultTheme {
+        textColor: string;
+        bgColor: string;
+        btnColor: string;
+    }
+}
