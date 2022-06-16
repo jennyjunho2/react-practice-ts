a6b424e2eb826a33036b4ba88165623caccb8118
jennyjunho2 <a01032208149@gmail.com>
2022-06-16 15:29:23 +0900

    220616

--- src/App.tsx
+++ src/App.tsx
@@ -56,9 +56,11 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
     background-color: ${props => props.theme.bgColor};
     color: ${props => props.theme.textColor};
+    line-height: 1.2;
   }
   
   a {
